cd2aa42ea249e349fd547e5aa1c387b703e47fb8
LidiaBaciu <lidia.baciu97@gmail.com>
2020-06-29 00:04:24 +0200

    removed unused methods for BeachController
    their functionality has been moved to specific methods and the result is now a property of the JSON response

--- buoy-app-server/src/main/java/com/example/coastmanagement/controller/BeachController.java
+++ buoy-app-server/src/main/java/com/example/coastmanagement/controller/BeachController.java
@@ -18,8 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -178,66 +176,5 @@ public class BeachController {
         beach.setPhotoUri(beachRequest.getPhotoUri());
         return beachRepository.save(beach);
     }
-    /*
-        Get the stats for today
-     */
-    @GetMapping("/beach/{id}/stats/today")
-    public HashMap<String, Float> getStatsForBeach(@PathVariable(value = "id") Long id){
-        Beach beach = beachRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Beach", "id", id));
-        Set<Buoy> buoys = beach.getBuoys();
-        HashMap<String, Float> stats = new HashMap<>();
-        //List<Float> stats = new ArrayList<>();
-        for (Buoy buoy: buoys) {
-            for(Sensor sensor : buoy.getSensors()){
-                for(SensorValue sensorValue : sensor.getSensorValues()){
-                    String sensorDate = sensorValue.getTimestamp();
-                    String dateNow = LocalDate.now().toString();
-                    System.out.println("sensorDate: " + sensorDate);
-                    System.out.println("dateNow: " + dateNow);
-                    if(sensorDate.contains(dateNow)){
-                        if(stats.containsKey(sensor.getName())){
-                            stats.put(sensor.getName(), (stats.get(sensor.getName()) + sensorValue.getValue())/2);
-                        }else{
-                            stats.put(sensor.getName(), sensorValue.getValue());
-                        }
-                    }
-                }
-            }
-        }
-        return stats;
-    }
 
-    @GetMapping("/beach/{id}/stats/yesterday")
-    public HashMap<String, Float> getStatsForBeachYesterday(@PathVariable(value = "id") Long id){
-        Beach beach = beachRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Beach", "id", id));
-        Set<Buoy> buoys = beach.getBuoys();
-        HashMap<String, Float> stats = new HashMap<>();
-        //List<Float> stats = new ArrayList<>();
-        for (Buoy buoy: buoys) {
-            for(Sensor sensor : buoy.getSensors()){
-                for(SensorValue sensorValue : sensor.getSensorValues()){
-                    String sensorDate = sensorValue.getTimestamp()
-                                        .substring(0, sensorValue.getTimestamp()
-                                        .indexOf(" "));
-                    String[] sensorDateParts = sensorDate.split("-");
-                    String dateNow = LocalDate.now().toString();
-                    String[] actualDateParts = dateNow.split("-");
-
-                    if(sensorDateParts[0].equals(actualDateParts[0])
-                            && sensorDateParts[1].equals(actualDateParts[1])
-                            && Integer.parseInt(sensorDateParts[2]) ==
-                                (Integer.parseInt(actualDateParts[2]) - 1)){
-                        if(stats.containsKey(sensor.getName())){
-                            stats.put(sensor.getName(), (stats.get(sensor.getName()) + sensorValue.getValue())/2);
-                        }else{
-                            stats.put(sensor.getName(), sensorValue.getValue());
-                        }
-                    }
-                }
-            }
-        }
-        return stats;
-    }
 }
